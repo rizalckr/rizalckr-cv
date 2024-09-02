4c0499a99a8058c5ffa44eefb3e1bfdceb2f606b
rizalckr <rizalckr@gmail.com>
2024-09-02 12:44:15 +0700

    Monday, 2/09/2024

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,10 +1,13 @@
 import styles from './HeroStyles.module.css';
 import heroImg from '../../assets/hero-img.png';
-import themeIcon from '../../assets/sun.svg'
+import themeIcon from '../../assets/sun.svg';
+import twitterIcon from '../../assets/twitter-light.svg';
+import githubIcon from '../../assets/github-light.svg';
+import linkedinIcon from '../../assets/linkedin-light.svg';
+import CV from '../../assets/cv.pdf';
 function Hero() {
   return (
-    <div>
-      <section id='hero'>
+      <section id='hero' className={styles.container}>
         <div className={styles.colorModeContainer}>
             <img className={styles.hero} 
             src={heroImg} 
@@ -15,8 +18,36 @@ function Hero() {
             alt='Color Mode'
             />
         </div>
+        <div className={styles.info}>
+          <h1>
+            Muh<br />
+            Amirul<br />
+            Rizal
+          </h1>
+          <h2>
+            Frontend Developer</h2>
+            <span>
+              <a href="#" target='_blank'>
+                <img src={twitterIcon} alt="Twitter" />
+              </a>
+              <a href="#" target='_blank'>
+                <img src={githubIcon} alt="Github" />
+              </a>
+              <a href="#" target='_blank'>
+                <img src={linkedinIcon} alt="LinkedIn" />
+              </a>
+            </span>
+            <p>
+              this is going to be edited, so for the time being this is
+              our text.
+            </p>
+            <a href={CV} target="_blank">
+              <button className='hover'>
+                Resume
+              </button>
+            </a>
+        </div>
       </section>
-    </div>
   )
 }
 
